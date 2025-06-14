55d7498d5d6cca59dfc32900769afff1346021b7
MatteoVentura <matteo.ventura18@gmail.com>
2025-06-14 09:00:26 +0200

    init project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Layouts
+import DefaultLayout from "./layouts/DefaultLayout";
+// Pages
+import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import PageNotFound from "./pages/PageNotFound";
+
+export default function App() {
+  return (
+    <AlertProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contacts" element={<ContactUs />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AlertProvider>
+  );
+}
